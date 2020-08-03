e9ed26595f0050e95c3b1edb86a013b91b2820f7
naracodes <naralee0124@gmail.com>
2020-08-03 17:32:59 -0400

    commit asset_id fix

--- app/controllers/api/watchlists_controller.rb
+++ app/controllers/api/watchlists_controller.rb
@@ -40,6 +40,6 @@ class Api::WatchlistsController < ApplicationController
     private
     def watchlist_params
         # params.require(:watchlist).permit(:ticker, :user_id => current_user.id)
-        params.permit(:ticker, :user_id, :asset_name, :latest_price)
+        params.permit(:ticker, :user_id, :asset_name, :latest_price, :asset_id)
     end
 end
--- app/views/api/watchlists/index.json.jbuilder
+++ app/views/api/watchlists/index.json.jbuilder
@@ -2,6 +2,6 @@
     # debugger
     json.set! watchlist_asset.ticker do
         # debugger
-        json.extract! watchlist_asset, :id, :user_id, :ticker, :asset_name, :latest_price
+        json.extract! watchlist_asset, :id, :user_id, :ticker, :asset_name, :latest_price, :asset_id
     end
 end
