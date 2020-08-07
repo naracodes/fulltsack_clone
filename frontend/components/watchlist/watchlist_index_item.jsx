3e0c24486e3affbeb171ddfcb49c4854dddf9a83
naracodes <naralee0124@gmail.com>
2020-08-07 10:00:42 -0400

    watchlist css

--- frontend/components/watchlist/watchlist_index_item.jsx
+++ frontend/components/watchlist/watchlist_index_item.jsx
@@ -5,8 +5,9 @@ const WatchlistIndexItem = props => {
     const { watchlistAsset } = props;
     debugger
     return (
-        <Link to={`/dashboard/${watchlistAsset.ticker}`}>
-            {watchlistAsset.ticker} {"    "} ${watchlistAsset.latest_price.toFixed(2)}
+        <Link to={`/dashboard/${watchlistAsset.ticker}`} id="watchlist-item">
+            <li>{watchlistAsset.ticker}</li>
+            <li>${watchlistAsset.latest_price.toFixed(2)}</li>
         </Link>
     )
 }
