564645061be51aaae217b063e9a3fa3ef7abf63d
naracodes <naralee0124@gmail.com>
2020-08-28 21:25:11 -0400

    add grid to dashboard

--- frontend/components/dashboard/dashboard.jsx
+++ frontend/components/dashboard/dashboard.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import NavBar from '../nav_bar/nav_bar'
 import LoginContainer from '../session/login_container';
-import AssetIndexContainer from '../assets/asset_index_container'
 import WatchlistIndexContainer from '../watchlist/watchlist_index_container';
 
 class Dashboard extends React.Component {
@@ -32,19 +31,10 @@ class Dashboard extends React.Component {
     const { currentUser, logout } = this.props;
     // debugger;
     const dashboard = currentUser ? (
-      <div className="dashboard-container">
-        <div className="search-outer">
-          <form className="search-container">
-            <input type="text" id="search-bar" onKeyDown={this.handleKeyDown} tabIndex="0"/>
-            <img className="search-icon" src="http://www.endlessicons.com/wp-content/uploads/2012/12/search-icon.png" />
-          </form>
-        </div>
-
-          <div className="watchlist-container">
-            <div className="watchlist-items">
+      <div className="dashboard-body">
+        <div className="dashboard-grid">
               <WatchlistIndexContainer />
-            </div>
-          </div>
+        </div>
       </div>
     ) : (
         <div>
--- frontend/components/watchlist/watchlist_index.jsx
+++ frontend/components/watchlist/watchlist_index.jsx
@@ -11,34 +11,23 @@ class WatchlistIndex extends React.Component {
         const { watchlistAssets, currentUser } = this.props;
         debugger
         return (
-          <div className="port-and-watch">
-                <div className="portfolio-container">
-                    {/* <h3>Your portfolio</h3>
-                    {watchlistAssets
-                    .reduce((acc, el) => {
-                    debugger;
-                    return acc + el.latest_price;
-                    }, 0)
-                    .toFixed(2)} */}
+          <div className="watchlist-items box">
+            <header id="list-title">
+              <h3>Watchlist</h3>
+            </header>
+            {watchlistAssets.map((watchlistAsset) => {
+              debugger;
+              return (
+                <div className="watchlist-item" key={watchlistAsset.id}>
+                  <li>
+                    <WatchlistIndexItem
+                      watchlistAsset={watchlistAsset}
+                      currentUser={currentUser}
+                    />
+                  </li>
                 </div>
-                <div className="inner-watch-list-container">
-                  <li id="list-title"><h3>Watchlist</h3></li>
-                  {watchlistAssets.map((watchlistAsset) => {
-                    debugger;
-                    return (
-                      <div className="watchlist2">
-                        <li>
-                            <WatchlistIndexItem
-                              key={watchlistAsset.id}
-                              watchlistAsset={watchlistAsset}
-                              currentUser={currentUser}
-                            />
-                        </li>
-
-                      </div>
-                    );
-                  })}
-                </div>
+              );
+            })}
           </div>
         );
     }
