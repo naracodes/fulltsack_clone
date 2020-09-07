32c04d2fc386c141e2d33d4fd898ae38e619c55d
naracodes <naralee0124@gmail.com>
2020-09-06 22:46:06 -0400

    start signup css

--- frontend/components/session/signup.jsx
+++ frontend/components/session/signup.jsx
@@ -40,65 +40,67 @@ class Signup extends React.Component {
 
   render() {
     return (
-      <div>
-        <div className="signup-home">
-          <Link to={"/"}>
-            <button className="home-btn">&#8962;</button>
-          </Link>
-        </div>
-        <div className="signup-container">
-          <div className="signup-form">
-            <div className="info">
-              <div className="signup-slogan">
-                <div className="slogan">Grow your piece of the pie.</div>
-                <p className="statement">
-                  Quiche is a new way to grow your money, offering a simple but
-                  dynamic platform for both traditional and alternative
-                  investing
-                </p>
+      <div className="signup-outermost">
+        <div className="signup-outermost-container"> 
+          <div className="signup-home">
+            <Link to={"/"}>
+              <button className="home-btn">&#8962;</button>
+            </Link>
+          </div>
+          <div className="signup-container">
+            <div className="signup-form">
+              <div className="info">
+                <div className="signup-slogan">
+                  <div className="slogan">Grow your piece of the pie.</div>
+                  <p className="statement">
+                    Quiche is a new way to grow your money, offering a simple but
+                    dynamic platform for both traditional and alternative
+                    investing
+                  </p>
+                </div>
               </div>
-            </div>
-            <form className="signup-subform">
-              <label>
-                <input
-                  type="text"
-                  placeholder={"First Name"}
-                  value={this.state.firstName}
-                  onChange={this.update("firstName")}
-                />
-              </label>
-              <label>
-                <input
-                  type="text"
-                  placeholder={"Last Name"}
-                  value={this.state.lastName}
-                  onChange={this.update("lastName")}
-                />
-              </label>
-              <label>
-                <input
-                  type="text"
-                  placeholder={"Email"}
-                  value={this.state.email}
-                  onChange={this.update("email")}
-                />
-              </label>
+              <form className="signup-subform">
+                <label>
+                  <input
+                    type="text"
+                    placeholder={"First Name"}
+                    value={this.state.firstName}
+                    onChange={this.update("firstName")}
+                  />
+                </label>
+                <label>
+                  <input
+                    type="text"
+                    placeholder={"Last Name"}
+                    value={this.state.lastName}
+                    onChange={this.update("lastName")}
+                  />
+                </label>
+                <label>
+                  <input
+                    type="text"
+                    placeholder={"Email"}
+                    value={this.state.email}
+                    onChange={this.update("email")}
+                  />
+                </label>
 
-              <label>
-                <input
-                  type="password"
-                  placeholder={"Password (min. 10 characters)"}
-                  value={this.state.password}
-                  onChange={this.update("password")}
-                />
-              </label>
-              <div >{this.renderErrors()}</div>
-              <button onClick={this.handleSubmit}>Continue</button>
-              <p className="message">
-                Already registered?
-                <Link to={"/login"}>Log in</Link>
-              </p>
-            </form>
+                <label>
+                  <input
+                    type="password"
+                    placeholder={"Password (min. 10 characters)"}
+                    value={this.state.password}
+                    onChange={this.update("password")}
+                  />
+                </label>
+                <div >{this.renderErrors()}</div>
+                <button onClick={this.handleSubmit}>Continue</button>
+                <p className="message">
+                  Already registered?
+                  <Link to={"/login"}>Log in</Link>
+                </p>
+              </form>
+            </div>
           </div>
         </div>
       </div>
