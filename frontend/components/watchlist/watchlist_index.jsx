93aaa695d0080c3510db7026d335cee056692772
naracodes <naralee0124@gmail.com>
2020-09-11 16:10:10 -0400

    style watchlist to incorporate mini graph

--- frontend/components/watchlist/watchlist_index.jsx
+++ frontend/components/watchlist/watchlist_index.jsx
@@ -3,29 +3,23 @@ import WatchlistIndexItem from './watchlist_index_item';
 
 class WatchlistIndex extends React.Component {
     componentDidMount() {
-        debugger
         this.props.fetchAllWatchlistAssets();
     }
 
     render() {
         const { watchlistAssets, currentUser } = this.props;
-        debugger
         return (
           <div className="watchlist-items box">
             <header id="list-title">
               <h3>Watchlist</h3>
             </header>
             {watchlistAssets.map((watchlistAsset) => {
-              debugger;
               return (
-                <div className="watchlist-item" key={watchlistAsset.id}>
-                  <div>
                     <WatchlistIndexItem
+                      key={watchlistAsset.id}
                       watchlistAsset={watchlistAsset}
                       currentUser={currentUser}
                     />
-                  </div>
-                </div>
               );
             })}
           </div>
--- frontend/components/watchlist/watchlist_index_item.jsx
+++ frontend/components/watchlist/watchlist_index_item.jsx
@@ -5,11 +5,18 @@ const WatchlistIndexItem = props => {
     const { watchlistAsset } = props;
     debugger
     return (
-        <div>
-            <Link to={`/stocks/${watchlistAsset.ticker}`}>
-                <span>{watchlistAsset.ticker}</span>
-                <span>${watchlistAsset.latest_price.toFixed(2)}</span>
-            </Link>
+        <div className="single-item">
+            {/* <Link to={`/stocks/${watchlistAsset.ticker}`} className="single-link">
+            </Link> */}
+                <div>
+                    <span>{watchlistAsset.ticker}</span>
+                </div>
+                <div>
+                    Graph
+                </div>
+                <div>
+                    <span>${watchlistAsset.latest_price.toFixed(2)}</span>
+                </div>
         </div>
     )
 }
