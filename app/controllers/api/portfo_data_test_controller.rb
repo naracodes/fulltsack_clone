d023ef542ecfc8ae950b02f5b623867503af18b4
naracodes <naralee0124@gmail.com>
2020-09-29 00:05:46 -0400

    finalize testing portfo data

--- app/controllers/api/portfo_data_test_controller.rb
+++ app/controllers/api/portfo_data_test_controller.rb
@@ -10,7 +10,7 @@ class Api::PortfoliosDataController < ApplicationController
         @last_portfo_data = PortfoData.last
         today = Time.now.strftime("%Y-%m-%d")
         market_open = "09:30 AM"
-        
+
         new_day = @last_portfo_data.create_at.strftime("%Y-%m-%d") < today
         if new_day
             today_open = Time.parse("9:30 AM")
@@ -19,7 +19,15 @@ class Api::PortfoliosDataController < ApplicationController
                 label: market_open,
                 cash_balance: current_user.cash_balance || User.find(46).cash_balance
             })
-        
+            until today_open.strftime("%I:%M %p") == "04:30 PM"
+                PortfoData.create({
+                    date: today,
+                    label: (today_open += (5 * 60)).strftime("%I:%M %p"),
+                    cash_balance: current_user.cash_balance || User.find(46).cash_balance
+                })
+            end
+            # create reset, weekend/holday instance variable methods
+            # today_open.reset!
         else
         end
     end
