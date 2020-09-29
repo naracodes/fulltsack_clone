1f9b304d4e5c81bda7616fb39e7412ec61805127
naracodes <naralee0124@gmail.com>
2020-09-28 23:22:00 -0400

    build logic to create portfo data every five min

--- app/controllers/api/portfo_data_test_controller.rb
+++ app/controllers/api/portfo_data_test_controller.rb
@@ -1,14 +1,19 @@
+require 'time'
+
 class Api::PortfoliosDataController < ApplicationController
     def index
         
     end
 
     def create
+        # t.strftime('%I:%M %p')
         @last_portfo_data = PortfoData.last
         today = Time.now.strftime("%Y-%m-%d")
         market_open = "09:30 AM"
+        
         new_day = @last_portfo_data.create_at.strftime("%Y-%m-%d") < today
         if new_day
+            today_open = Time.parse("9:30 AM")
             @first_of_day = PortfoData.create({
                 date: today,
                 label: market_open,
