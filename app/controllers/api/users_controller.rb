374a1d37c0909e3c457422b37ce1dfb0c79173b8
naracodes <naralee0124@gmail.com>
2020-10-16 01:29:38 -0400

    sample stocks for watchlist

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,5 +1,12 @@
+require 'iex-ruby-client'
+
 class Api::UsersController < ApplicationController
+
+
     def create
+        @current_user = current_user
+        rand_tickers = ["GOOGL", "FB", "TWTR", "GE", "AAPL", "NFLX", "STNG"]
+        rand_sample = rand_tickers.sample
         @user = User.new(user_params)
         if @user.save
             login!(@user)
@@ -8,6 +15,27 @@ class Api::UsersController < ApplicationController
                 transaction_type: "Deposit",
                 transaction_amount: 1000000,
             })
+            IEX::Api.configure do |config|
+                config.publishable_token = 'pk_9bae36c8264042f68549a11dc83620ac' # defaults to ENV['IEX_API_PUBLISHABLE_TOKEN']
+                config.endpoint = 'https://cloud.iexapis.com/v1'
+            end
+
+            client = IEX::Api::Client.new(
+            publishable_token: 'pk_9bae36c8264042f68549a11dc83620ac',
+            endpoint: 'https://cloud.iexapis.com/v1'
+            )
+            rand_tickers.shuffle.each do |ticker|
+                debugger
+                @quote = client.quote(ticker)
+                debugger
+                Watchlist.create({
+                    ticker: ticker,
+                    user_id: @current_user.id,
+                    asset_name: client.company(ticker).company_name,
+                    latest_price: @quote.latest_price,
+                    prev_close: @quote.previous_close,
+                })
+            end
             debugger
             render :show
         else
