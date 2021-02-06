f2c0292b5f2cab0594bd9a86d526b361a2251b29
naracodes <naralee0124@gmail.com>
2021-02-06 11:45:23 -0500

    add conditional for weekend to show Friday data, and change PortfoDatum query to fetch based on two where clauses

--- app/controllers/api/portfo_data_controller.rb
+++ app/controllers/api/portfo_data_controller.rb
@@ -3,17 +3,24 @@ class Api::PortfoDataController < ApplicationController
         portfo_data_params = params
         range = params["range"].upcase
         @current_user = current_user
+        @current_time = Time.current
         @data
         if range == "1D"
-            @data = PortfoDatum.where(created_at: Time.current.all_day, user_id: @current_user.id).order('created_at ASC')
+            if @current_time.saturday?
+                @data = PortfoDatum.where('created_at >= ?', 1.days.ago).where(user_id: @current_user.id).order('id ASC')
+            elsif @current_time.sunday?
+                @data = PortfoDatum.where('created_at >= ?', 2.days.ago).where(user_id: @current_user.id).order('id ASC')
+            else
+                @data = PortfoDatum.where(created_at: Time.current.all_day, user_id: @current_user.id).order('created_at ASC')
+            end
         elsif range == "1W"
-            @data = PortfoDatum.where('created_at >= ?', 1.week.ago, user_id: @current_user.id).order('created_at ASC')
+            @data = PortfoDatum.where('created_at >= ?', 1.week.ago).where(user_id: @current_user.id).order('created_at ASC')
         elsif range == "1M"
-            @data = PortfoDatum.where('created_at >= ?', 1.month.ago, user_id: @current_user.id).order('created_at ASC')
+            @data = PortfoDatum.where('created_at >= ?', 1.month.ago).where(user_id: @current_user.id).order('created_at ASC')
         elsif range == "3M"
-            @data = PortfoDatum.where('created_at >= ?', 3.months.ago, user_id: @current_user.id, label: "3:55PM").order('created_at ASC')
+            @data = PortfoDatum.where('created_at >= ?', 3.months.ago).where(user_id: @current_user.id, label: "3:55PM").order('created_at ASC')
         elsif range == "1Y"
-            @data = PortfoDatum.where('created_at >= ?', 12.months.ago, user_id: @current_user.id, label: "3:55PM").order('created_at ASC')
+            @data = PortfoDatum.where('created_at >= ?', 12.months.ago).where(user_id: @current_user.id, label: "3:55PM").order('created_at ASC')
         elsif range == "ALL"
             @data = PortfoDatum.where("DATE_PART('dow', created_at::date)=?", 2).where(label: "3:55 PM", user_id: @current_user.id).order('created_at ASC')
         end
@@ -25,3 +32,4 @@ class Api::PortfoDataController < ApplicationController
         params.permit(:range)
     end
 end
+
